af11dbea7c1d28a23466a1d9545b7eada70c9e20
Mike B <bowman.michael@gmail.com>
2024-08-17 23:04:36 -0400

    Adding basic error boundary

---
+++ movie-search-ui/src/components/ErrorBoundary/ErrorBoundary.tsx
@@ -0,0 +1,36 @@
+import React, { ReactNode } from 'react';
+
+interface Props {
+  children?: ReactNode;
+  fallback?: ReactNode;
+}
+
+interface State {
+  hasError: boolean;
+}
+
+class ErrorBoundary extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(_error: Error) {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: Error, errorInfo: React.ErrorInfo): void {
+    console.log(error);
+    console.log(errorInfo.componentStack?.toString());
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return this.props.fallback;
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
--- movie-search-ui/src/pages/_app.tsx
+++ movie-search-ui/src/pages/_app.tsx
@@ -2,6 +2,7 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Layout from 'src/components/Layout/Layout';
+import ErrorBoundary from 'src/components/ErrorBoundary/ErrorBoundary';
 
 const queryClient = new QueryClient();
 
@@ -9,7 +10,9 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
       <Layout>
-        <Component {...pageProps} />
+        <ErrorBoundary fallback={<div>The system is down!</div>}>
+          <Component {...pageProps} />
+        </ErrorBoundary>
       </Layout>
     </QueryClientProvider>
   );
